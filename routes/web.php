2a8b031e5f67802b271e28d186cc4ead14d6ad09
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-12-28 00:07:11 +0100

    feat(routes): add API resource routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
+
+Route::apiResource('tasks', TaskController::class);
+
