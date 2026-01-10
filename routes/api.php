283c587a37543eeaf5d11347ccb074d9ac2ef57c
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-01-10 17:51:04 +0100

    feat(Routes): add public authentication routes for user registration and login

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// ==========================[ PUBLIC AUTH ROUTES ] ==========================
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+// ==========================[ PROTECTED ROUTES ] ==========================
 
 Route::get('/user', function (Request $request) {
     return $request->user();
