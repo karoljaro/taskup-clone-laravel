7126d73108a5a8cc9dacf0bf16f27a1a3d6acd38
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-01-10 13:18:37 +0100

    feat(Providers): register Token and User repositories with dependency injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,18 @@
 
 namespace App\Providers;
 
+use App\Core\Application\Ports\TokenGenerator;
 use App\Core\Application\Ports\UnitOfWork as UnitOfWorkInterface;
 use App\Core\Application\Shared\IdGenerator as IdGeneratorInterface;
 use App\Core\Domain\Repositories\TaskRepository;
+use App\Core\Domain\Repositories\TokenRepository;
 use App\Core\Domain\Repositories\UserRepository;
 use App\Persistence\Eloquent\TaskEloquentModel;
 use App\Persistence\Eloquent\UserEloquentModel;
 use App\Persistence\Repositories\EloquentTaskRepository;
+use App\Persistence\Repositories\EloquentTokenRepository;
+use App\Persistence\Repositories\EloquentUserRepository;
+use App\Persistence\Services\SanctumTokenGenerator;
 use App\Persistence\Shared\EloquentUnitOfWork;
 use App\Persistence\Shared\UuidGenerator;
 use Illuminate\Support\ServiceProvider;
@@ -28,20 +33,34 @@ class AppServiceProvider extends ServiceProvider
         // ==========================[ REPOSITORIES ] ==========================
 
         // User Repository
-
-        // Token Repository
+        $this->app->singleton(
+            UserRepository::class,
+            fn() => new EloquentUserRepository(
+                $this->app->make(UserEloquentModel::class)
+            )
+        );
 
         // Task Repository
         $this->app->singleton(
             TaskRepository::class,
-            function () {
-                return new EloquentTaskRepository(new TaskEloquentModel());
-            }
+            fn() => new EloquentTaskRepository(
+                $this->app->make(TaskEloquentModel::class)
+            )
+        );
+
+        // Token Repository
+        $this->app->singleton(
+            TokenRepository::class,
+            EloquentTokenRepository::class
         );
 
         // ==========================[ APPLICATION SERVICES ] ==========================
 
         // Token Generator Service
+        $this->app->singleton(
+            TokenGenerator::class,
+            SanctumTokenGenerator::class
+        );
 
         // ==========================[ UNIT OF WORK ] ==========================
 
